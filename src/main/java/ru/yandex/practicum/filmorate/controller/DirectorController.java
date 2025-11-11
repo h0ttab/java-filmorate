8ec9e169c15c89c232c3125785355f9098000c9f
h0ttab <myemail@example.com>
2025-11-11 17:58:22 +0300

    feat(director): Add director controller

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.model.dto.film.DirectorDto;
+import ru.yandex.practicum.filmorate.service.DirectorService;
+
+@RestController
+@RequestMapping("/directors")
+@RequiredArgsConstructor
+public class DirectorController {
+    private final DirectorService directorService;
+
+    @GetMapping
+    public List<Director> findAll() {
+        return directorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Director findById(@PathVariable Integer id) {
+        return directorService.findById(id);
+    }
+
+    @PostMapping
+    public Director create(@RequestBody DirectorDto directorDto) {
+        return directorService.create(Director.builder().name(directorDto.getName()).build());
+    }
+
+    @PutMapping
+    public Director update(@RequestBody Director director) {
+        return directorService.update(director);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        directorService.delete(id);
+    }
+}
