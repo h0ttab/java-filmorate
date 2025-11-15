43a9a6108b98c24145b6788ec646ef5490f81e1d
Petr <red19650226@yandex.ru>
2025-11-15 04:02:07 +0300

    style: Cleanup

--- src/test/java/ru/yandex/practicum/filmorate/service/ReviewServiceIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/ReviewServiceIntegrationTest.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum.filmorate.service;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +13,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.model.dto.review.ReviewCreateDto;
 import ru.yandex.practicum.filmorate.model.dto.review.ReviewUpdateDto;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
