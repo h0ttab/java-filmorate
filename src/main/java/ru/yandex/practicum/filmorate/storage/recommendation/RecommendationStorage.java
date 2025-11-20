75905df5062af86f4cd8dfe5cc7e3ad302b0536b
nikitanovikov <Vertihost@yandex.com>
2025-11-20 22:52:15 +0300

    Fix(Ревью): Удалил неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/storage/recommendation/RecommendationStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/recommendation/RecommendationStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.recommendation;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Интерфейс для хранилища рекомендаций фильмов.
